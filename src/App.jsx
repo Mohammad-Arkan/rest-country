3bdb91be4e0a55a9c947749673ade922036cc324
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-06-22 16:02:59 +0600

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Header from "./component/Header";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+     <Header/>
+     <Outlet/>
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './component/Home.jsx';
+import About from './component/About.jsx';
+import Countries from './component/Countries.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path:"/countries",
+        element:<Countries/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
